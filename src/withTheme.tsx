411b031d8dbfd5d7243d71e81f194af6c6557847
Damian Baar <damian.baar@gmail.com>
2019-01-14 18:06:55 +0100

    bulma-styled-theme: changing to function component instead of class, sassy-react-component: accepting functional component as an input to styledWithVariants, improving autogeneration for os projects

--- src/withTheme.tsx
+++ src/withTheme.tsx
@@ -1,33 +1,21 @@
-import React from 'react'
+import React, { ReactChild, ComponentType } from 'react'
 import { defaultTheme as basicTheme, BulmaTheme } from '@hungry/bulma-theme'
 import { normalizeCSS } from '@hungry/bulma-normalize'
-import { ThemeProvider } from 'styled-components'
+import baseStyled, { ThemeProvider as StyledThemeProvider, ThemedStyledInterface } from 'styled-components'
 
 // @ts-ignore
 const attachNormalize = normalizeCSS
 
-// TODO export styled function with embedded bulma theme
-export class BasicThemeProvider extends React.Component<{ theme?: BulmaTheme }> {
-  public static displayName = 'BulmaThemeProvider'
-
-  public render() {
-    const {
-      theme,
-      children,
-    } = this.props
-
-    return (
-      <ThemeProvider
-        theme={(theme || basicTheme)}
-      >
-        {children as React.ReactChild}
-      </ThemeProvider>
-    )
-  }
-}
+export const ThemeProvider =
+  ({ theme, children }: { theme?: BulmaTheme, children: ComponentType<unknown> | JSX.Element }) =>
+    <StyledThemeProvider theme={(theme || basicTheme)} >
+      {children as ReactChild}
+    </StyledThemeProvider>
 
 export class Theme {
-  public static Provider = BasicThemeProvider
+  public static Provider = ThemeProvider
 }
 
 export { withTheme } from 'styled-components'
+export type BulmaStyledTheme<K> = ThemedStyledInterface<BulmaTheme & K>
+export const styled = baseStyled as BulmaStyledTheme<{}>
